d4326fe4e2c45eb92c3dbbb4ec125bf1e94a694b
Eman Alibalic <therealemanmane@gmail.com>
2022-12-08 11:43:18 +0100

    HabitatDao created

---
+++ src/main/java/ba/unsa/etf/rpr/dao/HabitatDao.java
@@ -0,0 +1,10 @@
+package ba.unsa.etf.rpr.dao;
+import ba.unsa.etf.rpr.domain.Habitat;
+
+/**
+ * Dao interface for Category domain bean
+ *
+ * @author Eman Alibalić
+ */
+public interface HabitatDao extends Dao<Habitat> {
+}
