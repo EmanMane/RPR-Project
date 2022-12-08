912e2029b2f5e0d40d005868ca6d6099afce65ca
Eman Alibalic <therealemanmane@gmail.com>
2022-12-08 11:28:54 +0100

    ZooHabitats update

---
+++ src/main/java/ba/unsa/etf/rpr/domain/Habitat.java
@@ -0,0 +1,51 @@
+package ba.unsa.etf.rpr.domain;
+
+import java.util.Objects;
+
+/**
+ * List of possible categories for habitat
+ *
+ * @author Eman Alibalić
+ */
+public class Habitat {
+
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Habitat category = (Habitat) o;
+        return id == category.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
