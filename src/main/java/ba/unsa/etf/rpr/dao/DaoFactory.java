66f277760a3cdf797ce1db7df9e0e9e9152a6f40
Eman Alibalic <therealemanmane@gmail.com>
2022-12-24 10:23:11 +0100

    DaoFactory implemented

--- src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
+++ src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
@@ -1,4 +1,23 @@
 package ba.unsa.etf.rpr.dao;
 
 public class DaoFactory {
+    private static final HabitatDao habitatDao = new HabitatDaoSQLImpl();
+    private static final AnimalDao animalDao = new AnimalDaoSQLImpl();
+    private static final AnimalHistoryDao animalHistoryDao = new AnimalHistoryDaoSQLImpl();
+
+    private DaoFactory(){
+    }
+
+    public static HabitatDao habitatDao(){
+        return habitatDao;
+    }
+
+    public static AnimalDao animalDao(){
+        return animalDao;
+    }
+
+    public static AnimalHistoryDao animalHistoryDao(){
+        return animalHistoryDao;
+    }
+
 }
