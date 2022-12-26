5394ae7163afc8cbb37d800b7f4e8fe0672fc12c
Eman Alibalic <therealemanmane@gmail.com>
2022-12-26 16:39:08 +0100

    small updates

--- src/main/java/ba/unsa/etf/rpr/AppFX.java
+++ src/main/java/ba/unsa/etf/rpr/AppFX.java
@@ -1,11 +1,13 @@
 package ba.unsa.etf.rpr;
 
 import ba.unsa.etf.rpr.controllers.HomeController;
+import ba.unsa.etf.rpr.controllers.LoginController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
@@ -15,14 +17,16 @@ import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class AppFX extends Application {
     @Override
-    public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/home.fxml"));
-        HomeController homeController = new HomeController();
-        fxmlLoader.setController(homeController);
-        Parent root = fxmlLoader.load();
-        stage.setTitle("Zoo v1.0");
-        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
-        stage.setResizable(false);
-        stage.show();
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+        primaryStage.initStyle(StageStyle.UNDECORATED); //Adobe type Login (No resize)
+        primaryStage.setTitle("Zoo v1.0");
+        primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args){
+        launch(args);
     }
 }
