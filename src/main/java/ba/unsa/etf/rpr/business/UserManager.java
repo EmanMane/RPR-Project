bee693eb745040da1f932fcddada0c301189f60a
Eman Alibalic <therealemanmane@gmail.com>
2022-12-27 01:25:22 +0100

    User business layer

---
+++ src/main/java/ba/unsa/etf/rpr/business/UserManager.java
@@ -0,0 +1,62 @@
+package ba.unsa.etf.rpr.business;
+
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Animal;
+import ba.unsa.etf.rpr.domain.AnimalHistory;
+import ba.unsa.etf.rpr.domain.User;
+import ba.unsa.etf.rpr.exceptions.AnimalException;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Business Logic Layer for Users
+ *
+ * @author Eman Alibalić
+ */
+public class UserManager {
+
+    public List<User> getAll() throws AnimalException {
+        return DaoFactory.userDao().getAll();
+    }
+
+    public List<User> searchUser(String text) throws AnimalException {
+        return DaoFactory.userDao().searchByName(text);
+    }
+
+    public boolean validateUser(String username,String password) throws AnimalException{
+        if (password == null || username==null){
+            throw new AnimalException("No username or password!");
+        }
+        else {
+            List<User> x = new ArrayList<User>();
+            x = searchUser(username);
+            for (User u : x) {
+                if (u.getUsername().equals(username)) {
+                    if (u.getPassword().equals(password)) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+
+    public void delete(int id) throws AnimalException{
+        DaoFactory.userDao().delete(id);
+    }
+
+    public User getById(int userId) throws AnimalException{
+        return DaoFactory.userDao().getById(userId);
+    }
+
+    public void update(User x) throws AnimalException{
+        DaoFactory.userDao().update(x);
+    }
+
+    public User add(User x) throws AnimalException{
+        return DaoFactory.userDao().add(x);
+    }
+
+}
