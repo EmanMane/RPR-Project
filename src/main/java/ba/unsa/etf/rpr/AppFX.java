be0cb7bed9bea2f667a09455953551e8acc17f9c
Eman Alibalic <therealemanmane@gmail.com>
2023-01-13 22:52:49 +0100

    appfx cleanup

--- src/main/java/ba/unsa/etf/rpr/AppFX.java
+++ src/main/java/ba/unsa/etf/rpr/AppFX.java
@@ -1,6 +1,5 @@
 package ba.unsa.etf.rpr;
 
-import ba.unsa.etf.rpr.controllers.HomeController;
 import ba.unsa.etf.rpr.controllers.LoginController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -8,7 +7,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 /**
@@ -16,20 +14,15 @@ import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
  */
 
 public class AppFX extends Application {
-    /**
-     * start method is used to literally start the App
-     * with the first scene being composed through login.fxml
-     */
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
-        primaryStage.initStyle(StageStyle.UNDECORATED); //Adobe type Login (No resize)
-        primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
-        primaryStage.setResizable(false);
-        primaryStage.show();
-    }
-
-    public static void main(String[] args){
-        launch(args);
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
+        LoginController loginController = new LoginController();
+        fxmlLoader.setController(loginController);
+        Parent root = fxmlLoader.load();
+        stage.initStyle(StageStyle.UNDECORATED); //Adobe type Login (No resize)
+        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setResizable(false);
+        stage.show();
     }
 }
