89eade04d19713c802dd24b5753c4cf67c0c5de1
Eman Alibalic <therealemanmane@gmail.com>
2023-01-14 21:36:56 +0100

    Idable comments edit

--- src/main/java/ba/unsa/etf/rpr/domain/Idable.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Idable.java
@@ -1,7 +1,8 @@
 package ba.unsa.etf.rpr.domain;
 
 /**
- * Interface that forces all POJO beans to have ID field that can be accessed with setID() and getID()
+ * Interface that forces all POJO beans to have ID field that can be accessed with
+ * two methods being setID() and getID()
  */
 public interface Idable{
     void setId(int id);
