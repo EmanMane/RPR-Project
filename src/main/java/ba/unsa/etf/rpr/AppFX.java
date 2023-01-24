b0ec4c0e686f6799ee7380adc6953db0514d5199
Eman Alibalic <therealemanmane@gmail.com>
2023-01-24 22:34:24 +0100

    Icon added

--- src/main/java/ba/unsa/etf/rpr/AppFX.java
+++ src/main/java/ba/unsa/etf/rpr/AppFX.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
@@ -18,6 +19,7 @@ public class AppFX extends Application {
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
         LoginController loginController = new LoginController();
+        stage.getIcons().add(new Image("/jpeg/SanDiegoZooLogo.png"));
         fxmlLoader.setController(loginController);
         Parent root = fxmlLoader.load();
         stage.initStyle(StageStyle.UNDECORATED); //Adobe type Login (No resize)
