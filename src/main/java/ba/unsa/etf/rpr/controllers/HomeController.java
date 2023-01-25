e033255b8dd7d47ef48c973cb371b0af92fb262c
Eman Alibalic <therealemanmane@gmail.com>
2023-01-25 01:43:18 +0100

    Home fix (stage switch)

--- src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
@@ -7,18 +7,23 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+import static javafx.scene.layout.Region.USE_PREF_SIZE;
 
 public class HomeController {
 
-    public Label animalLabel;
+    @FXML
+    public BorderPane BP;
 
     private AnimalManager manager = new AnimalManager();
 
@@ -28,11 +33,36 @@ public class HomeController {
     }
 
     public void signOutApp(ActionEvent actionEvent){
-        openDialog("Login", "/fxml/login.fxml", new LoginController());
+        try {
+            ((Stage) BP.getScene().getWindow()).hide();
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
+            LoginController loginController = new LoginController();
+            Stage stage = new Stage();
+            stage.getIcons().add(new Image("/jpeg/SanDiegoZooLogo.png"));
+            fxmlLoader.setController(loginController);
+            Parent root = fxmlLoader.load();
+            stage.initStyle(StageStyle.UNDECORATED); //Adobe type Login (No resize)
+            stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+            stage.setResizable(false);
+            stage.show();
+        }catch (Exception e){
+            new Alert(Alert.AlertType.NONE, e.getMessage(), ButtonType.OK).show();
+        }
     }
 
     public void openAbout(ActionEvent actionEvent){
-        openDialog("About", "/fxml/about.fxml", null);
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/about.fxml"));
+            loader.setController(null);
+            Stage stage = new Stage();
+            stage.setScene(new Scene(loader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+            stage.setTitle("About");
+            stage.setResizable(false);
+            stage.initStyle(StageStyle.UTILITY);
+            stage.show();
+        }catch (Exception e){
+            new Alert(Alert.AlertType.NONE, e.getMessage(), ButtonType.OK).show();
+        }
     }
 
     public void openEditHabitats(ActionEvent event){
@@ -52,6 +82,8 @@ public class HomeController {
             stage.setTitle(title);
             stage.initStyle(StageStyle.UTILITY);
             stage.show();
+            stage.setMinHeight(600);
+            stage.setMinWidth(610);
         }catch (Exception e){
             new Alert(Alert.AlertType.NONE, e.getMessage(), ButtonType.OK).show();
         }
